5d2df7f7599c47940c7c3b78ac0920d21e5b2be9
hirogit1002 <hiromokada@gmail.com>
2019-01-30 13:13:12 +0100

    release

--- program/run.py
+++ program/run.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Running')
 parser.add_argument('--path','-d', default="../data/*.jpg", type=str)
 parser.add_argument('--path_test','-td', default="../data_test/*.jpg", type=str)
 parser.add_argument('--train','-t', default=1, type=int)
-parser.add_argument('--test','-v', default=1, type=int)
+parser.add_argument('--test','-v', default=0, type=int)
 parser.add_argument('--test_size','-ts', default=10, type=int)
 parser.add_argument('--batch_size','-b', default=10, type=int)
 parser.add_argument('--model','-m', default= "AE", type=str)
@@ -22,7 +22,9 @@ paths = np.array(glob.glob(args.path))
 paths_test = np.array(glob.glob(args.path_test))
 
 if(args.train):
+    print('Train start')
     y_value,out=train_network(paths, args.test_size, args.batch_size,args.init,args.latent, args.norm,[-1, 64, 64, 1], args.epochs, args.model,"../logs")
 
 if(args.test):
-    y_value, z = test_network(paths_test, args.latent, args.norm,[-1, 64, 64, 1], args.model)
+    print('Test start')
+    test_network(paths_test, args.latent, args.norm,[-1, 64, 64, 1], args.model)
