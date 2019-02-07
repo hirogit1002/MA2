ee77a678ff92caa82775b585f39daa12970042ac
hirogit1002 <hiromokada@gmail.com>
2019-02-07 16:59:15 +0100

    program

--- program/test_dcgan.py
+++ program/test_dcgan.py
@@ -14,22 +14,11 @@ from imgproc import*
 
 def test_network_gan(test_size, latent_size, normalizarion, lr):
     tf.reset_default_graph()
-    perm = np.random.permutation(len(data))
-    data = np.array(data[perm])
-    train_data = np.array(data[:-test_size])
-    test_data = np.array(data[-test_size:])
     weight_path = '../weigths/'+'DCGAN' + '.ckpt'
     n_test = test_size
-    x = tf.placeholder(tf.float32, [None, 64, 64, 1], name='InputData')
     z = tf.placeholder(tf.float32, [None, latent_size], name='latent')
     Training = tf.placeholder(dtype=tf.bool, name='LabelData')
     generated = decoder(z,Training)
-    sig, D_logits = discriminator(x,Training, reuse=False)
-    sig_, D_logits_ = discriminator(generated, Training, reuse=True)
-    loss_real, loss_fake, g_loss, val_loss_real, val_loss_fake, val_g_loss = loss_gan(D_logits,D_logits_)                    
-    d_loss = loss_real + loss_fake
-    val_d_loss = val_loss_real + val_loss_fake
-
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         sess.run(tf.global_variables_initializer())
         # create log writer object
@@ -38,11 +27,6 @@ def test_network_gan(test_size, latent_size, normalizarion, lr):
         y_values = []
         zs = []
         for i in data:
-            imgs = np.array(Image.open(i).convert('L'))
-            imgs = imgs[np.newaxis,:,:,np.newaxis]
-            if (normalizarion):
-                imgs = imgs/255.
-            
             sampled = sample_z(1, latent_size)
             feed = {z: sampled, x: test_imgs, Training:False}  
             output, test_d_cost, test_g_cost = sess.run([generated], feed_dict = feed)
