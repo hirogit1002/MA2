7bce26f17cb959ae889bff765171038322311cc3
hirogit1002 <hiromokada@gmail.com>
2019-02-26 21:32:01 +0100

    programm

--- program/supervised_learning.py
+++ program/supervised_learning.py
@@ -19,7 +19,7 @@ class SVM():
         self.emos_inv = {1:'anger',2:'contempt',3:'disgust',4:'fear',5:'happy',6:'sad',7:'surprise'}
         print('Data loaded')
         self.imgs_path = np.array(sorted(glob.glob('../data_test/*.jpg')))
-        self.imgs = np.array([np.array(Image.open(i).convert('L')) for i in self.imgs_path]
+        self.imgs = np.array([np.array(Image.open(i).convert('L')) for i in self.imgs_path])
         self.vectors, self.y_value, self.y = self.load(path_vector,path_y_value,path_labels)
         self.y_value = np.array(self.y_value)
         self.y = np.array(self.y[:,0])
