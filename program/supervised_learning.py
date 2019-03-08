87d1f3891b62ac8af30317d816218a090e012cbe
hirogit1002 <hiromokada@gmail.com>
2019-03-08 11:21:23 +0100

    programm

--- program/supervised_learning.py
+++ program/supervised_learning.py
@@ -333,9 +333,9 @@ class SVM():
             plt.imshow(value[i],cmap='gray')
         plt.show()
         
-    def split_again(self,perm):
+    def split_again(self,perm,Test_size):
         n = len(perm)
-        self.X_train, self.X_test, self.y_train, self.y_test, self.y_value_train, self.y_value_test, self.img_train, self.img_test, self.n_test=split(n, self.vectors[perm], self.y[perm], self.y_value[perm], self.imgs[perm] ,self.test_size)
+        self.X_train, self.X_test, self.y_train, self.y_test, self.y_value_train, self.y_value_test, self.img_train, self.img_test, self.n_test=split(n, self.vectors[perm], self.y[perm], self.y_value[perm], self.imgs[perm] , Test_size)
     
     def cv_again(self, Test_size=0.3):
         self.X_train, self.X_test, self.y_train, self.y_test, self.y_value_train, self.y_value_test, self.img_train, self.img_test, self.n_test, self.perm = cv(self.vectors, self.y, self.y_value ,self.imgs, Test_size)
