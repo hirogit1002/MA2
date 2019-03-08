0a32f5755cdd3ae696b46ce91446c6759ed86428
hirogit1002 <hiromokada@gmail.com>
2019-03-08 12:24:04 +0100

    programm

--- program/supervised_learning.py
+++ program/supervised_learning.py
@@ -336,7 +336,8 @@ class SVM():
         plt.show()
         
     def split_again(self,perm):
-        Test_size = self.n_test/len(self.y)
+        n = len(self.y)
+        Test_size = self.n_test/n
         self.X_train, self.X_test, self.y_train, self.y_test, self.y_value_train, self.y_value_test, self.img_train, self.img_test, self.n_test=split(n, self.vectors[perm], self.y[perm], self.y_value[perm], self.imgs[perm] , Test_size)
     
     def cv_again(self, Test_size=0.3):
